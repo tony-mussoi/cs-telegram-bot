9203b3b6abedf34d806c0c7775c884da694a5f15
tony-mussoi <mussoi@cubostart.com>
2025-06-26 11:46:33 -0300

    commit 26Jun2025

---
+++ cs_chatbot_lib.py
@@ -0,0 +1,195 @@
+import os
+import logging
+import requests
+import json
+from pyairtable import Table
+
+AIRTABLE_ACCESS_TOKEN = os.environ.get('AIRTABLE_ACCESS_TOKEN')
+AIRTABLE_ENDPOINT = 'https://api.airtable.com/v0/'
+headers = {'Authorization': f'Bearer {AIRTABLE_ACCESS_TOKEN}', 'Content-Type': 'application/json'}
+
+# Retrieve Bots configurations, grouped by phone number
+def fetch_client_bots(base_id, table_name):
+    url = f'{AIRTABLE_ENDPOINT}{base_id}/{table_name}'
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        print("Error fetching data:", response.status_code)
+        return {}
+    records = response.json().get('records', [])
+    user_sessions = {}
+    # Set in-memory User Sessions
+    for record in records:
+        fields = record.get('fields', {})
+        client_phone = fields.get('From number', '').strip()
+        chat_id = fields.get('Chat ID', '').strip()
+        clients = fields.get('Clients', '')
+        companies = fields.get('Companies', '')
+        authorized = fields.get('Authorized', '')
+        if authorized:
+            stage = 'initial_stage'
+        elif not client_phone:
+            stage = 'auth_awaiting_start'
+        else:
+            stage = 'auth_awaiting_authorization'
+        # Add individual session to sessions dictionary
+        if client_phone not in user_sessions:
+            user_sessions[chat_id] = {
+                'chat_id': chat_id,
+                'client_phone': client_phone,
+                'authorized': authorized,
+                'clients': clients,
+                'companies': [],
+                'stage': stage,
+                'selected_company': None,
+                'selected_theme': None,
+            }
+        user_sessions[chat_id]['companies'].append(companies)
+    return user_sessions
+
+# Find a record in an Airtable table by field value and return its ID.
+def find_record_id_by_value(base_id, table_name, field_name, field_value):
+    # Initialize the Airtable client with the base ID and table name
+    airtable = Table(AIRTABLE_ACCESS_TOKEN, base_id, table_name)
+    try:
+        # Fetch all records from the table
+        records = airtable.all()
+        # Loop through the records to find the one that matches the field value
+        for record in records:
+            # Check if the field value matches the value you are searching for
+            if record['fields'].get(field_name) == field_value:
+                # Return the record ID if a match is found
+                return record['id']
+        # Return None if no record is found
+        return None
+    except Exception as e:
+        print(f"An error occurred while searching for the record: {e}")
+    return None
+
+# Get individual Airtable record
+def get_airtable_record(base_id, table_name, record_id):
+    url = f'{AIRTABLE_ENDPOINT}{base_id}/{table_name}/{record_id}'
+    # Send your request and parse the response
+    response = requests.get(url, headers=headers)
+    data = json.loads(response.text)
+    # Print the field values for the record
+    print(data['fields'])
+    return data
+
+# Create new record in Airtable
+def create_airtable_record(base_id, table_name, data):
+    url = f'{AIRTABLE_ENDPOINT}{base_id}/{table_name}'
+    # Send a POST request to create the record and parse response
+    # print("Data being sent to Airtable:")
+    # print(json.dumps(data, indent=4))  # This will help you inspect the data structure
+    response = requests.post(url, headers=headers, json=data)
+    # Check if the request was successful
+    if response.status_code == 200:
+        print("Record created successfully:", response.json())
+    else:
+        print("Failed to create record:", response.json())
+    return response.status_code
+
+# Get file URL from Airtable
+def get_file_url_from_airtable(base_id, table_name, filename):
+    url = f'{AIRTABLE_ENDPOINT}{base_id}/{table_name}'
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        return None
+    for record in response.json().get("records", []):
+        company_name = record.get("fields", {}).get("Company", [])
+        attachments = record.get("fields", {}).get("Tax ID file", [])
+        for file in attachments:
+            #if file.get("filename", "").lower() == filename.lower():
+            if file.get("filename", "") == filename:
+                return file.get("url")
+    return None
+
+# Add Chatbot user to Airtable
+def add_bot_user(base_id, table_name, phone, user_id, request_name, request_client):
+    url = f'{AIRTABLE_ENDPOINT}{base_id}/{table_name}'
+    # Check if the phone already exists
+    filter_formula = f"{{From number}} = '{phone}'"
+    check_url = f"{url}?filterByFormula={requests.utils.quote(filter_formula)}"
+    check_response = requests.get(check_url, headers=headers)
+    check_response.raise_for_status()
+    records = check_response.json().get("records", [])
+    # Setup user data
+    data_fields = {
+            "From number": phone,
+            "Chat ID": str(user_id),
+            "Request Name": request_name,
+            "Request Client": request_client,
+    }
+    # Update the existing record in Airtable
+    if records: # If User already exists
+        record_id = records[0]["id"]
+        patch_url = f"{url}/{record_id}"
+        patch_response = requests.patch(patch_url, headers=headers, json={"fields": data_fields})
+        patch_response.raise_for_status()
+        return patch_response.status_code
+
+    # Create a new record, in Airtable, if not found
+    post_response = requests.post(url, headers=headers, json={"fields": data_fields})
+    post_response.raise_for_status()
+    return post_response.status_code
+
+# Add in-memory Chatbot user Session
+def add_new_session(user_sessions, chat_id):
+    user_sessions[chat_id] = {
+        'chat_id': chat_id,
+        'client_phone': '',
+        'authorized': '',
+        'clients': '',
+        'companies': [],
+        'stage': 'auth_awaiting_start',
+        'selected_company': None,
+        'selected_theme': None,
+    }
+    return user_sessions
+
+# Check User bot authorization
+def is_user_authorized_OLD(base_id, table_name, user_id, session):
+    url = f'{AIRTABLE_ENDPOINT}{base_id}/{table_name}'
+    params = {
+        "filterByFormula": f"{{Chat ID}} = '{user_id}'"
+    }
+    response = requests.get(url, headers=headers, params=params)
+    response.raise_for_status()
+    records = response.json().get("records", [])
+    if not records or not records[0]["fields"].get("Authorized"):
+        session['Authorized'] = False
+    else:
+        session['Authorized'] = True
+    return session
+
+
+def is_user_authorized(base_id, table_name, user_id, session):
+    url = f'{AIRTABLE_ENDPOINT}{base_id}/{table_name}'
+    params = {
+        "filterByFormula": f"{{Chat ID}} = '{user_id}'"
+    }
+    try:
+        response = requests.get(url, headers=headers, params=params)
+        response.raise_for_status()
+        records = response.json().get("records", [])
+        if not records:
+            session['authorized'] = False
+            return session
+        fields = records[0].get("fields", {})
+        authorized = fields.get("Authorized", False)
+        session['authorized'] = authorized
+        # If authorized, retrieve Clients and Companies
+        if authorized:
+            clients_str = fields.get("Clients", "")[0]
+            session['clients'] = [client.strip() for client in clients_str.split(",")] if clients_str else []
+            companies_str = fields.get("Companies", "")[0]
+            session['companies'] = [company.strip() for company in companies_str.split(",")] if companies_str else []
+            # Update session stage, if still within Authorization process
+            if session['stage'] in ['auth_awaiting_start', 'auth_awaiting_name', 'auth_awaiting_phone',
+                                    'auth_awaiting_authorization']:
+                session['stage'] = 'initial_stage'
+
+    except requests.RequestException as e:
+        logging.error(f"Error checking user authorization: {e}")
+        session['authorized'] = False
+    return session
--- cs_telegram_bot.py
+++ cs_telegram_bot.py
@@ -1,175 +1,237 @@
-import logging
-from telegram import Update
-from telegram.ext import Application, CommandHandler, MessageHandler, ContextTypes, filters
-from whatsapp_lib import *
-#import telegram
-#print(telegram.__version__)
-#help(Application.run_webhook)
+from telegram import Update, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telegram.ext import (ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters)
+from cs_chatbot_lib import *
 
-# Logging
+# Setup Logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-# Load Telegram app Token, Webhook URL, and Airtable Base ID
-TELEGRAM_APP_TOKEN = os.environ["TELEGRAM_APP_TOKEN"]
+# --- Environment Variables ---
+TELEGRAM_APP_TOKEN = os.environ.get("TELEGRAM_APP_TOKEN")
 WEBHOOK_URL = os.environ["WEBHOOK_URL"]  # e.g., https://abc123.ngrok-free.app
 PORT = int(os.environ.get("PORT", 5000))
-BASE_ID = os.environ.get('AIRTABLE_CRM_OPS_BASE_ID')
+ADMIN_CHAT_ID = os.environ.get("ADMIN_CHAT_ID")
+MONITORING_CHAT_ID = int(os.environ.get("MONITORING_CHAT_ID"))
+AIRTABLE_CRM_OPS_BASE_ID = os.environ.get('AIRTABLE_CRM_OPS_BASE_ID')
 
-# ✅ Setup variables for Webhook flow control
-# User selection lists
+# --- Load User Sessions ---
+user_sessions = fetch_client_bots(AIRTABLE_CRM_OPS_BASE_ID, 'Bots')
+
+# --- Shared Options ---
 theme_options = ['Corporate', 'Tax']
-corp_data_fields = ['Company type','Company Legal Name', 'Incorporation Country']
-tax_data_fields = ['EIN','Tax Country', 'Tax forms']
+corp_data_fields = ['Company type', 'Company Legal Name', 'Incorporation Country']
+tax_data_fields = ['EIN', 'Tax Country', 'Tax forms']
 
-# Lists for display
-theme_options_list = "\n".join(f"{i + 1}. {company}" for i, company in enumerate(theme_options))
-corp_data_fields_list = "\n".join(f"{i + 1}. {company}" for i, company in enumerate(corp_data_fields))
-tax_data_fields_list = "\n".join(f"{i + 1}. {company}" for i, company in enumerate(tax_data_fields))
-
-# Standard message strings
-noloco_link = "\n\nFor full access, login to:\n https://cubostart-crm-portal.noloco.co/"
-hello_string = "Hello \'{}\'! Please, select the company:\n{}" + noloco_link
-invalid_string = "Sorry, invalid selection."
-
-# Initiate User sessions
-# States: 'awaiting_client_name', 'awaiting_company_name', 'awaiting_theme_selection', 'awaiting_data_request'
-user_sessions = fetch_client_bots(BASE_ID, 'Bots')
-telegram_sessions = {}  # memory session: telegram_id -> session
-
-
-# Get Client´s session from phone number originating the Whatsapp message
-def get_session_from_phone(client_number):
-    for session in user_sessions.values():
-        if session.get('client_phone') == client_number:
-            return session
-    return None
-
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    user_id = str(update.effective_user.id)
-    if user_id in telegram_sessions:
-        del telegram_sessions[user_id]
-    await update.message.reply_text("Session reset. Please type anything to begin again.")
-
-
-# Parse message when a selection list is presented for the user
+# Parse bot user selected options
 def parse_option_selection(s):
     s = s.strip()
-    return int(s) if s in {str(i) for i in range(10)} else 0
+    return int(s) if s.isdigit() and 0 <= int(s) <= 9 else 0
 
+# Control Bot workflow, and handle data requests
+def run_flow_control(session, incoming_msg):
+    base_id = AIRTABLE_CRM_OPS_BASE_ID
+    string, file_url = None, None
 
-# ✅ Setup general Telegram Handlers
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    user_id = str(update.effective_user.id)
-    if user_id in telegram_sessions:
-        del telegram_sessions[user_id]
-    await update.message.reply_text("Session reset. Please type anything to begin again.")
+    if not session.get('authorized'):
+        string = "You're not authorized yet. Please wait for approval."
+        return string, None
 
-
-# ✅ Setup Telegram main Message Handler
-async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    user_id = str(update.effective_user.id)
-    incoming_msg = update.message.text.strip()
-    session = telegram_sessions.get(user_id)
-
-    if not session:
-        for s in user_sessions.values():
-            if s.get("telegram_user_id") == user_id:
-                session = s.copy()
-                break
-        if not session:
-            await update.message.reply_text("Sorry, we couldn't find your user in our system.")
-            return
-        session['stage'] = 'initial_stage'
-        telegram_sessions[user_id] = session
-
-    company_list = "\n".join(f"{i + 1}. {c}" for i, c in enumerate(session['companies']))
-    string = ""
-    file_url = None
-
-    stage = session['stage']
-    msg = incoming_msg.lower()
-
-    if stage == 'initial_stage':
+    if session['stage'] == 'initial_stage':
+        company_list = "\n".join(f"{i + 1}. {c}" for i, c in enumerate(session["companies"]))
         session['stage'] = 'awaiting_company_selection'
-        string = f"Hello {session['client_name']}! Please, select the company:\n{company_list}" + noloco_link
+        string = f"Hello {session['clients']}! Please select the company:\n{company_list}".\
+            replace('[', '').replace(']', '')
 
-    elif stage == 'awaiting_company_selection':
-        sel = parse_option_selection(msg)
+    elif session['stage'] == 'awaiting_company_selection':
+        sel = parse_option_selection(incoming_msg)
         if sel == 0:
-            string = f"Please, select the company:\n{company_list}" + noloco_link
+            session['stage'] = 'awaiting_company_selection'
         elif sel <= len(session['companies']):
             session['selected_company'] = session['companies'][sel - 1]
             session['stage'] = 'awaiting_theme_selection'
-            string = f"Which data segment are you looking for in {session['selected_company']}?\n{theme_options_list}\n0. to select another Company"
+            string = f"Which data segment are you looking for {session['selected_company']}?\n" + \
+                     "\n".join(f"{i + 1}. {t}" for i, t in enumerate(theme_options)) + "\n0. to select another Company"
         else:
-            string = invalid_string
+            string = "Invalid selection."
 
-    elif stage == 'awaiting_theme_selection':
-        sel = parse_option_selection(msg)
+    elif session['stage'] == 'awaiting_theme_selection':
+        sel = parse_option_selection(incoming_msg)
         if sel == 0:
             session['stage'] = 'awaiting_company_selection'
-            string = f"Please, select the company:\n{company_list}" + noloco_link
         elif sel <= len(theme_options):
             session['selected_theme'] = theme_options[sel - 1]
             session['stage'] = 'awaiting_data_request'
-            field_list = corp_data_fields_list if session['selected_theme'] == 'Corporate' else tax_data_fields_list
-            string = f"Which {session['selected_theme']} data are you looking for '{session['selected_company']}'?\n{field_list}\n0. to select another Company"
+            if session['selected_theme'] == 'Corporate':
+                string = f"Which Corporate data are you looking for '{session['selected_company']}'?\n" + \
+                    "\n".join(f"{i+1}. {f}" for i, f in enumerate(corp_data_fields)) + "\n0. to select another Company"
+            else:
+                string = f"Which Tax data are you looking for '{session['selected_company']}'?\n" + \
+                    "\n".join(f"{i+1}. {f}" for i, f in enumerate(tax_data_fields)) + "\n0. to select another Company"
         else:
-            string = invalid_string
+            string = "Invalid selection."
 
-    elif stage == 'awaiting_data_request':
-        sel = parse_option_selection(msg)
+    elif session['stage'] == 'awaiting_data_request':
+        sel = parse_option_selection(incoming_msg)
         company_name = session['selected_company']
         theme = session['selected_theme']
-        company_id = find_record_id_by_value(BASE_ID, 'Companies', 'Company', company_name)
-        company_dict = get_airtable_record(BASE_ID, 'Companies', company_id)
+        company_id = find_record_id_by_value(base_id, 'Companies', 'Company', company_name)
+        company_dict = get_airtable_record(base_id, 'Companies', company_id)
         fields = {k.lower(): v for k, v in company_dict.get('fields', {}).items()}
 
         if sel == 0:
             session['stage'] = 'awaiting_company_selection'
-            string = f"Please, select the company:\n{company_list}" + noloco_link
+            company_list = "\n".join(f"{i + 1}. {c}" for i, c in enumerate(session['companies']))
+            string = f"Hello {session['client_name']}! Please select the company:\n{company_list}"
+
         elif theme == 'Corporate' and sel <= len(corp_data_fields):
-            field = corp_data_fields[sel - 1].lower()
-            value = fields.get(field, 'Not available')
+            lookup = corp_data_fields[sel - 1].lower()
+            value = fields.get(lookup)
             string = f"{corp_data_fields[sel - 1]}: {value}"
-        elif theme == 'Tax' and sel <= len(tax_data_fields):
+
+        elif theme == 'Tax':
             if sel == 1:
-                t1 = fields.get('tax id type', 'N/A')
-                t2 = fields.get('tax id number', 'N/A')
-                string = f"Tax ID type: {t1}  -  Number: {t2}"
-                file_url = get_file_url_from_airtable(BASE_ID, 'Companies', company_name + ' - EIN.pdf')
-            else:
-                field = tax_data_fields[sel - 1].lower()
-                value = fields.get(field, 'Not available')
+                value_1 = fields.get('tax id type')
+                value_2 = fields.get('tax id number')
+                string = f"Tax ID type: {value_1}  -  Number: {value_2}"
+                file_url = get_file_url_from_airtable(base_id, 'Companies', company_name + ' - EIN.pdf')
+            elif sel <= len(tax_data_fields):
+                lookup = tax_data_fields[sel - 1].lower()
+                value = fields.get(lookup)
                 string = f"{tax_data_fields[sel - 1]}: {value}"
-        else:
-            string = invalid_string
+            else:
+                string = "Invalid selection."
 
-    else:
-        string = "Sorry, I’m not sure what to do. Type /start to reset."
+    return string, file_url
 
-    await update.message.reply_text(string)
+# --- Telegram Handlers ---
+# Handle general messages
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user = update.effective_user
+    user_id = str(user.id)
+    session = user_sessions.get(user_id)
+    incoming_msg = update.message.text.strip()
+
+    if not user_sessions or session['stage'] == 'auth_awaiting_start':
+        await update.message.reply_text("You're not authorized yet. Please type command '/start'.")
+        return
+
+    if session['stage'] == 'auth_awaiting_name':
+        session['client_name'] = incoming_msg
+        session['stage'] = 'auth_awaiting_phone'
+        button = KeyboardButton("\ud83d\udcf1 Share my phone number", request_contact=True)
+        keyboard = ReplyKeyboardMarkup([[button]], resize_keyboard=True, one_time_keyboard=True)
+        await update.message.reply_text("Thank you, now tap the keyboard icon at the bottom right "
+                                        "to share your phone number.", reply_markup=keyboard)
+        return
+
+    if session['stage'] == 'auth_awaiting_phone':
+        await update.message.reply_text("Please tap the keyboard icon at the bottom right to send your phone number.")
+        return
+
+    session = is_user_authorized(AIRTABLE_CRM_OPS_BASE_ID, "Bots", user_id, session)
+    if not session.get('authorized'):
+        await update.message.reply_text("You're not authorized yet. Please wait while we review your access.")
+        return
+
+    # Handle message, retrieving required data, and manage current workflow
+    string, file_url = run_flow_control(session, incoming_msg)
+    if string:
+        await update.message.reply_text(string)
     if file_url:
-        await update.message.reply_document(document=file_url)
+        await update.message.reply_document(file_url)
+
+    # Log messages to monitoring Chat
+    if MONITORING_CHAT_ID:
+        await context.bot.send_message(chat_id=MONITORING_CHAT_ID,
+                                text=f"From: {user_id}\n" f"Msg: '{incoming_msg}'\n" f"Stage: {session.get('stage')}")
+        await context.bot.send_message(chat_id=MONITORING_CHAT_ID, text=f"Bot replied to `{user_id}`:\n{string}",)
     return
 
+# Handle phone number, from contact message
+async def contact_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_id = str(update.effective_user.id)
+    session = user_sessions.get(user_id)
+    if session['stage'] == 'auth_awaiting_start':
+        await update.message.reply_text("You're not authorized yet. Please type command '/start'.")
+        return
+    # Initialize Chatbot user session
+    phone = update.message.contact.phone_number
+    request_name = update.effective_user.first_name + " " + update.effective_user.last_name
+    session['phone'] = phone
+    session['authorized'] = False
+    session['stage'] = 'auth_awaiting_authorization'
+    request_client = session.get('client_name', '')
 
-# ✅ Application setup
-def main():
-    application = Application.builder().token(TELEGRAM_APP_TOKEN).build()
-    # Application Handlers
-    application.add_handler(CommandHandler("start", start))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+    # Add user to table Bots @ Airtable, if it does not exist, and wait for manual authorization
+    add_bot_user(AIRTABLE_CRM_OPS_BASE_ID, "Bots", phone, user_id, request_name, request_client)
+    await update.message.reply_text("Thank you! Please wait while we review your access request.")
+    if ADMIN_CHAT_ID:
+        await context.bot.send_message(chat_id=ADMIN_CHAT_ID,
+            text=f"*New user waiting authorization:*\n👤 {request_name} @ {request_client}\n📞 {phone}\n🆔 {user_id}",
+                                       parse_mode="Markdown")
+    return
 
-    print(f">> Starting webhook at: {WEBHOOK_URL}/webhook")
-    application.run_webhook(
-        listen="0.0.0.0",
-        port=PORT,
-        url_path="webhook",
-        webhook_url=f"{WEBHOOK_URL}/webhook"
-    )
+# Handle '/start' command
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    global user_sessions
+    user_id = str(update.effective_user.id)
+    # Check for existing Chatbot user session
+    if (not user_sessions) or (not user_sessions.get(user_id)):
+        # Add user session; stage = 'auth_awaiting_start'
+        user_sessions = add_new_session(user_sessions, user_id)
 
+    # Initialize in-memory Session
+    session = user_sessions.get(user_id)
+    session = is_user_authorized(AIRTABLE_CRM_OPS_BASE_ID, "Bots", user_id, session)
+    if session['stage'] == 'auth_awaiting_authorization':
+        await update.message.reply_text("Your request is already under review. You´ll be notified when completed.")
+    else:
+        await update.message.reply_text("Welcome to Cubo Bot! Please type your Client or Company name:")
+        session['stage'] = 'auth_awaiting_name'
+    return
 
-if __name__ == "__main__":
-    main()
+# Handle '/reset_bot' command
+async def reset_bot(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    global user_sessions
+    user_id = str(update.effective_user.id)
+    if user_id == ADMIN_CHAT_ID:
+        user_sessions = fetch_client_bots(AIRTABLE_CRM_OPS_BASE_ID, 'Bots')
+    return
+
+######## MAIN ########
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(TELEGRAM_APP_TOKEN).build()
+    application.add_handler(CommandHandler('start', start))
+    application.add_handler(CommandHandler('reset_bot', reset_bot))
+    application.add_handler(MessageHandler(filters.CONTACT, contact_handler))
+    application.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), handle_message))
+    application.run_webhook(listen='0.0.0.0', port=PORT, url_path='webhook', webhook_url=f'{WEBHOOK_URL}/webhook')
+###### END MAIN ######
+
+### Auxiliary Handlers
+#import telegram
+#print(telegram.__version__)
+#help(Application.run_webhook)
+
+#async def debug_chat_id(update: Update, context: ContextTypes.DEFAULT_TYPE):
+#    await update.message.reply_text(f"Chat ID: {update.effective_chat.id}")
+
+#async def get_my_id(update: Update, context: ContextTypes.DEFAULT_TYPE):
+#    await update.message.reply_text(f"Your Telegram user ID is: {update.effective_user.id}")
+
+# application.add_handler(CommandHandler("chatid", debug_chat_id))
+# application.add_error_handler(error_handler)
+
+"""
+#from telegram.error import TelegramError
+# Handle bot errors
+async def error_handler(update: object, context: ContextTypes.DEFAULT_TYPE) -> None:
+    logging.error(msg="Exception while handling an update:", exc_info=context.error)
+    if isinstance(update, Update) and update.effective_chat:
+        try:
+            await context.bot.send_message(
+                chat_id=update.effective_chat.id,
+                text="⚠️ An internal error occurred. The admins have been notified.",
+            )
+        except TelegramError as e:
+            logging.error(f"Failed to send error message to user: {e}")
+"""
